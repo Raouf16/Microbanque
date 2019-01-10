0884fd05ff93a5e41441a5289cc657c58c8b989d
Raouf16 <haddad.raouf@live.fr>
2019-01-10 22:00:34 +0100

    First commit to initialize project

---
+++ src/main/java/fr/dauphine/miageif/msa/Microbanque/MicrobanqueApplication.java
@@ -0,0 +1,14 @@
+package fr.dauphine.miageif.msa.Microbanque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrobanqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrobanqueApplication.class, args);
+	}
+
+}
+
